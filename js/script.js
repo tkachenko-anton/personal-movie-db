b8e8cef5439436a2382776b15c9947b9fc245a3f
Anton <tkachenko.da.bucha@gmail.com>
2020-11-19 16:56:21 +0200

    Improvements to script.js

--- js/script.js
+++ js/script.js
@@ -10,12 +10,29 @@ const personalMovieDB = {
   privat: false
 };
 
-const question1 = prompt('Один из последних просмотренных фильмов?', ''),
-question2 = prompt('На сколько оцените его?', ''),
-question3 = prompt('Один из последних просмотренных фильмов?', ''),
-question4 = prompt('На сколько оцените его?', ''); 
+for (let i = 0; i < 2; i++) {
+  const question1 = prompt('Один из последних просмотренных фильмов?', ''),
+        question2 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[question1] = question2;
-personalMovieDB.movies[question3] = question4;
+  if (question1 != null && question2 != null && question1 != '' && question2 != '' && question1.length < 50) {
+    personalMovieDB.movies[question1] = question2;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    console.log("Вы классический зритель");
+  } else if (personalMovieDB.count >= 30){
+    console.log("Вы киноман");
+  } else {
+    console.log("Произошла ошибка");
+  }
+  
+
 
 console.log(personalMovieDB);
